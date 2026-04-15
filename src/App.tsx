917fdbaef27a457d5d617316d8230fba65cc8ab3
aziz <azizjalal951@gmail.com>
2026-04-15 15:37:24 +0800

    fix: add basename to browser router

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* Tambahkan basename agar router mengenali sub-folder repository kamu */}
+      <BrowserRouter basename="/golden-vows-studio">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
